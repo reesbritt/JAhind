cc68522eeaf6d844cb7209321f4903993bc82b5b
Rees Britt <rees@clippd.io>
2023-04-05 14:50:49 +0100

    add scripts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,12 @@
 import TagManager  from 'react-gtm-module'
 import { Head, Html, Main, NextScript } from 'next/document'
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 const tagManagerArgs = {
   gtmID: 'GTM-P6DK3L7',
 }
+const metaPixelId = '222932703738150';
 
 
 export default function Document() {
@@ -20,13 +22,34 @@ export default function Document() {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init(constants.siteMeta.FacebookPixelID);
+        ReactPixel.init(metaPixelId);
         ReactPixel.pageView();
       });
   }, [])
   return (
     <Html className="scroll-smooth bg-white antialiased" lang="en">
       <Head>
+        <Script id='gtm'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${tagManagerArgs.gtmID}');`
+          }
+        </Script>
+        <Script id='meta-pixel'>
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${metaPixelId}');
+            fbq('track', 'PageView');
+            `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
