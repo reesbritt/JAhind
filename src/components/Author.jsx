b04bfb6fcdefeb4db41db9cdfcf33b5a1edf0c61
Rees Britt <rees@clippd.io>
2023-10-26 22:49:30 +0100

    update author image and embed new media for radio interviews

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -3,17 +3,13 @@ import Link from 'next/link'
 
 import { GridPattern } from '@/components/GridPattern'
 import { SectionHeading } from '@/components/SectionHeading'
-import authorImage from '@/images/avatars/author.jpg'
+import authorImage from '@/images/avatars/author-raw.jpeg'
 import { Expandable } from './Expandable'
 
 function InstaIcon(props) {
   return (
-    <svg
-    fill="currentColor"
-    viewBox="0 0 24 24"
-    {...props}>
-      <path
-        d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+    <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
     </svg>
   )
 }
@@ -48,7 +44,7 @@ export function Author() {
         <div className="bg-slate-50 pt-px sm:rounded-6xl">
           <div className="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-slate-200 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
             <Image
-              className="absolute inset-0 h-full w-full object-cover"
+              className="absolute inset-0 h-full w-full object-cover object-top"
               src={authorImage}
               alt=""
               sizes="(min-width: 1024px) 18rem, (min-width: 768px) 16rem, 11rem"
@@ -60,27 +56,50 @@ export function Author() {
             </SectionHeading>
             <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
               <span className="block text-blue-600">JA Hind –</span> A bit about
-               the author behind &apos;Humans in the Extreme&apos;.
+              the author behind &apos;Humans in the Extreme&apos;.
             </p>
             <Expandable>
-          {({ isExpanded }) => {
-            const additionalText = "This tragic account of human suffering, loss and fortitude has instilled in Jane a valuable life lesson. Not only in terms of the fragility of our existence, but also the indomitable spirit of the human being, when faced with their own mortality. Another humbling lesson that her grandfather’s incredible bravery has taught her is that where it may be easy to be selfless when things are going well, the real test is often when we are up against the odds. Although the circumstances that most of us will face in a lifetime will rarely come close to what happened to Jane’s Grandfather, the question as to what it is that drives one person to lay their life on the line for others and another to ‘abandon ship’, so to speak, is very much at the heart of her book, Humans in the Extreme. Her Grandfather’s resolution and compassion, in the face of 'extreme' has, she feels, also orientated her professionally.";
-            const additionalSecondParagraph = "Prior to becoming a psychotherapist, Jane launched a career in nursing; supporting and navigating patients and their families through often life-changing traumas. Being able to make a difference in other people’s lives continues to be a major part of her work as a psychotherapist. In addition to her therapy work, Jane has also lectured students in counselling at Herts Regional College in Ware and facilitated training workshops for NHS staff and schools in Hertfordshire. Connecting with people from all walks of life who have often experienced the most difficult of circumstances remains an integral part of her work. Over the years, she has worked with victims of physical and/or sexual violence and offered clinical support to ex-offenders of violence and sexual abuse in a bid to reduce and manage any risk of them further offending. Whilst her encounters with perpetrators of violence have surged in recent years, so too has her interest to understand our human relationship with the extreme, in all its forms. It is because of this that Jane decided to move beyond the doorstep of her native subject psychology and explore those other influencing factors that can sway the human condition. By researching and reviewing the work of many renowned scientific and historical writers, she has been able to assemble a more comprehensive ‘take’ on the concept of extreme whilst putting a psychological frame to it.";
-            return (<>
-            <p className="mt-4 text-lg tracking-tight text-slate-700">
-            Jane Amelda Hind was born in North London, England and is the second youngest of seven children. Her parents, Maureen and Charles Feeney, originated from Cleggan, which is a small fishing village on the west coast of Ireland. Where Cleggan holds much appeal – especially for writers and artists, the most notable being the poet Richard Murphy - it is also etched in tragedy. For it was on a night in October 1927 that 45 local fishermen lost their lives to a severe and most unexpected storm, now known as ‘The Cleggan Bay Disaster’. Festus Feeney, the captain of one of the fishing boats, was the only man able to steer his boat home safely that night and, in doing so, saved not just himself but also the lives of his entire crew. This story has much pertinence to Jane because Festus Feeney was her Grandfather.<br/>
-            { isExpanded && (
-              <>
-                {additionalText}
-                <br/>
-                {additionalSecondParagraph}
-              </>
-            )}
-            </p>
-              {!isExpanded && <Expandable.Button>Click here to read more</Expandable.Button>}
-            </>)
-          }}
-        </Expandable>
+              {({ isExpanded }) => {
+                const additionalText =
+                  "This tragic account of human suffering, loss and fortitude has instilled in Jane a valuable life lesson. Not only in terms of the fragility of our existence, but also the indomitable spirit of the human being, when faced with their own mortality. Another humbling lesson that her grandfather’s incredible bravery has taught her is that where it may be easy to be selfless when things are going well, the real test is often when we are up against the odds. Although the circumstances that most of us will face in a lifetime will rarely come close to what happened to Jane’s Grandfather, the question as to what it is that drives one person to lay their life on the line for others and another to ‘abandon ship’, so to speak, is very much at the heart of her book, Humans in the Extreme. Her Grandfather’s resolution and compassion, in the face of 'extreme' has, she feels, also orientated her professionally."
+                const additionalSecondParagraph =
+                  'Prior to becoming a psychotherapist, Jane launched a career in nursing; supporting and navigating patients and their families through often life-changing traumas. Being able to make a difference in other people’s lives continues to be a major part of her work as a psychotherapist. In addition to her therapy work, Jane has also lectured students in counselling at Herts Regional College in Ware and facilitated training workshops for NHS staff and schools in Hertfordshire. Connecting with people from all walks of life who have often experienced the most difficult of circumstances remains an integral part of her work. Over the years, she has worked with victims of physical and/or sexual violence and offered clinical support to ex-offenders of violence and sexual abuse in a bid to reduce and manage any risk of them further offending. Whilst her encounters with perpetrators of violence have surged in recent years, so too has her interest to understand our human relationship with the extreme, in all its forms. It is because of this that Jane decided to move beyond the doorstep of her native subject psychology and explore those other influencing factors that can sway the human condition. By researching and reviewing the work of many renowned scientific and historical writers, she has been able to assemble a more comprehensive ‘take’ on the concept of extreme whilst putting a psychological frame to it.'
+                return (
+                  <>
+                    <p className="mt-4 text-lg tracking-tight text-slate-700">
+                      Jane Amelda Hind was born in North London, England and is
+                      the second youngest of seven children. Her parents,
+                      Maureen and Charles Feeney, originated from Cleggan, which
+                      is a small fishing village on the west coast of Ireland.
+                      Where Cleggan holds much appeal – especially for writers
+                      and artists, the most notable being the poet Richard
+                      Murphy - it is also etched in tragedy. For it was on a
+                      night in October 1927 that 45 local fishermen lost their
+                      lives to a severe and most unexpected storm, now known as
+                      ‘The Cleggan Bay Disaster’. Festus Feeney, the captain of
+                      one of the fishing boats, was the only man able to steer
+                      his boat home safely that night and, in doing so, saved
+                      not just himself but also the lives of his entire crew.
+                      This story has much pertinence to Jane because Festus
+                      Feeney was her Grandfather.
+                      <br />
+                      {isExpanded && (
+                        <>
+                          {additionalText}
+                          <br />
+                          {additionalSecondParagraph}
+                        </>
+                      )}
+                    </p>
+                    {!isExpanded && (
+                      <Expandable.Button>
+                        Click here to read more
+                      </Expandable.Button>
+                    )}
+                  </>
+                )
+              }}
+            </Expandable>
             <p className="mt-8">
               <Link
                 href="mailto:jane.hind@nhs.net"
@@ -99,7 +118,6 @@ export function Author() {
                 <span className="ml-4">Follow on Instagram</span>
               </Link>
             </p>
-            
           </div>
         </div>
       </div>
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,23 +1,159 @@
-import Image from 'next/image'
-
 import { Container } from '@/components/Container'
 import { SectionHeading } from '@/components/SectionHeading'
-import abstractBackgroundImage from '@/images/resources/abstract-background.png'
-import discordImage from '@/images/resources/discord.svg'
-import figmaImage from '@/images/resources/figma.svg'
-import videoPlayerImage from '@/images/resources/video-player.svg'
 
 const resources = [
   {
     title: 'Mix 92.6 Radio Interview',
     description:
-      'Listen to a 10 minute radio interview about Humans in the extreme, with author JA Hind.',
+      'Listen to a 10 minute radio interview about Humans in the extreme, with author JA Hind on Mix 92.6.',
     image: function FigmaImage() {
       return (
-        <><iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1502094901%3Fsecret_token%3Ds-ZYWy80aZ5Qh&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-        <div style={{fontSize: '10px', color: '#cccccc',lineBreak: 'anywhere', wordBreak: 'normal',overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: '100'}}>
-          <a href="https://soundcloud.com/jahind" title="JA Hind" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}} rel="noreferrer">JA Hind</a> · 
-          <a href="https://soundcloud.com/jahind/jahind-human-in-the-extreme-radio-interview/s-ZYWy80aZ5Qh" title="JAHind- Human in the Extreme - Radio Interview" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}} rel="noreferrer">JAHind- Human in the Extreme - Radio Interview</a></div></>
+        <>
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1502094901%3Fsecret_token%3Ds-ZYWy80aZ5Qh&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          ></iframe>
+          <div
+            style={{
+              fontSize: '10px',
+              color: '#cccccc',
+              lineBreak: 'anywhere',
+              wordBreak: 'normal',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              fontFamily:
+                'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',
+              fontWeight: '100',
+            }}
+          >
+            <a
+              href="https://soundcloud.com/jahind"
+              title="JA Hind"
+              target="_blank"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+              rel="noreferrer"
+            >
+              JA Hind
+            </a>{' '}
+            ·
+            <a
+              href="https://soundcloud.com/jahind/jahind-human-in-the-extreme-radio-interview/s-ZYWy80aZ5Qh"
+              title="JAHind- Human in the Extreme - Radio Interview"
+              target="_blank"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+              rel="noreferrer"
+            >
+              JAHind- Human in the Extreme - Radio Interview
+            </a>
+          </div>
+        </>
+      )
+    },
+  },
+  {
+    title: 'BBC Interview: Humans in the Extreme',
+    description:
+      'Listen to an interview with Babs Michel on BBC Three Counties Radio, with author JA Hind about Humans in the Extreme.',
+    image: function FigmaImage() {
+      return (
+        <>
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1650190674&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          ></iframe>
+          <div
+            style={{
+              fontSize: '10px',
+              color: '#cccccc',
+              lineBreak: 'anywhere',
+              wordBreak: 'normal',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              fontFamily:
+                'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',
+              fontWeight: '100',
+            }}
+          >
+            <a
+              href="https://soundcloud.com/jahind"
+              title="JA Hind"
+              target="_blank"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+              rel="noreferrer"
+            >
+              JA Hind
+            </a>{' '}
+            ·
+            <a
+              href="https://soundcloud.com/jahind/ja-hind-bbc-humans-in-the-extreme"
+              title="JAHind- Human in the Extreme - Radio Interview"
+              target="_blank"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+              rel="noreferrer"
+            >
+              JA Hind - BBC: Humans in the Extreme
+            </a>
+          </div>
+        </>
+      )
+    },
+  },
+  {
+    title: 'BBC Interview: How do you beat loneliness',
+    description:
+      'Listen to an interview with Honor Morrison, with author JA Hind about beating loneliness on BBC Three Counties Radio.',
+    image: function FigmaImage() {
+      return (
+        <>
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1650190674&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+          ></iframe>
+          <div
+            style={{
+              fontSize: '10px',
+              color: '#cccccc',
+              lineBreak: 'anywhere',
+              wordBreak: 'normal',
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              fontFamily:
+                'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',
+              fontWeight: '100',
+            }}
+          >
+            <a
+              href="https://soundcloud.com/jahind"
+              title="JA Hind"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+            >
+              JA Hind
+            </a>{' '}
+            ·{' '}
+            <a
+              href="https://soundcloud.com/jahind/ja-hind-bbc-humans-in-the-extreme"
+              title="JA Hind - BBC: Humans in the Extreme"
+              style={{ color: '#cccccc', textDecoration: 'none' }}
+            >
+              JA Hind - BBC: Humans in the Extreme
+            </a>
+          </div>
+        </>
       )
     },
   },
@@ -35,27 +171,21 @@ export function Resources() {
           Media
         </SectionHeading>
         <p className="mt-8 font-display text-4xl font-bold tracking-tight text-slate-900">
-          Learn more about Humans in the Extreme by exploring some more media from JA Hind.
+          Learn more about Humans in the Extreme by exploring some more media
+          from JA Hind.
         </p>
-        {/* <p className="mt-4 text-lg tracking-tight text-slate-700">
-          Want to know a bit more? Explore some of the content below to discover
-        </p> */}
       </Container>
       <Container size="lg" className="mt-16">
         <ol
           role="list"
           className="-mx-3 grid grid-cols-1 gap-y-10 lg:grid-cols-3 lg:text-center xl:-mx-12 xl:divide-x xl:divide-slate-400/20"
         >
-          <li></li>
           {resources.map((resource) => (
             <li
               key={resource.title}
               className="grid auto-rows-min grid-cols-1 items-center gap-8 px-3 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-1 xl:px-12"
             >
-              <div className="relative h-48 overflow-hidden rounded-2xl shadow-lg sm:h-60 lg:h-40">
-                <resource.image />
-              </div>
-              <div>
+              <div className="h-24">
                 <h3 className="text-base font-medium tracking-tight text-slate-900">
                   {resource.title}
                 </h3>
@@ -63,9 +193,11 @@ export function Resources() {
                   {resource.description}
                 </p>
               </div>
+              <div className="relative h-48 overflow-hidden rounded-2xl shadow-lg sm:h-60 lg:h-40">
+                <resource.image />
+              </div>
             </li>
           ))}
-          <li></li>
         </ol>
       </Container>
     </section>
